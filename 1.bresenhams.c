bcf7a00cb1af9a36287de9f2041a3e878190fa81
SillyPill <akumar42@protonmail.com>
2018-05-29 22:27:53 +0530

    Fixed some error

--- 1.bresenhams.c
+++ 1.bresenhams.c
@@ -8,9 +8,7 @@ void display()
 {
 	int i, j;
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1.0, 1.0, 1.0);
-	void line(x00, y00, xEnd, yEnd);
-	glFlush();
+	line(x00, y00, xEnd, yEnd);
 }
 
 void drawPixel(int x, int y)
@@ -19,6 +17,7 @@ void drawPixel(int x, int y)
 	glBegin(GL_POINTS);
 	glVertex2i(x, y);
 	glEnd();
+	glFlush();
 }
 
 void line(int x00, int y00, int xEnd, int yEnd)
@@ -56,19 +55,20 @@ void line(int x00, int y00, int xEnd, int yEnd)
 
 void myinit()
 {
-	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glClearColor(0.4, 0.4, 0.4, 1.0);
 	glPointSize(2.0);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
+	glColor3f(0.5,1,0.5);
+	/* glMatrixMode(GL_PROJECTION); */
+	/* glLoadIdentity(); */
 	gluOrtho2D(0.0, 950.0, 0.0, 950.0);
 }
 
 void main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
+
 	printf("Enter two end points of the line");
 	scanf("%d %d %d %d", &x00, &y00, &xEnd, &yEnd);
-
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(10, 10);
@@ -77,3 +77,4 @@ void main(int argc, char** argv)
 	myinit();
 	glutMainLoop();
 }
+
