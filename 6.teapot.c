4ef7196cb3b711ac37526f7d30c8ff411949420c
SillyPill <akumar42@protonmail.com>
2018-05-29 22:47:32 +0530

    Changed to the smaller, more difficult to remember program

--- 6.teapot.c
+++ 6.teapot.c
@@ -1,111 +1,50 @@
 /* Simple shaded scene with a teapot */
 #include <GL/glut.h>
 
-void wall(double thickness)
+void obj(double tx, double ty, double tz, double sx, double sy,
+		double sz)
 {
-	glPushMatrix();
-	glTranslated(0.5, 0.5 * thickness, 0.5);
-	glScaled(1.0, thickness, 1.0);
+	glRotated(50, 0, 1, 0);
+	glRotated(10, -1, 0, 0);
+	glRotated(11.7, 0, 0, -1);
+	glTranslated(tx, ty, tz);
+	glScaled(sx, sy, sz);
 	glutSolidCube(1.0);
-	glPopMatrix();
-}
-
-void tableLeg(double thick, double len)
-{
-	glPushMatrix();
-	glTranslated(0, len / 2, 0);
-	glScaled(thick, len, thick);
-	glutSolidCube(1.0);
-	glPopMatrix();
-}
-void table(double topWid, double topThick, double legThick, double legLen)
-{
-	glPushMatrix();
-
-	glTranslated(0, legLen, 0);
-	glScaled(topWid, topThick, topWid);
-	glutSolidCube(1.0);
-
-	glPopMatrix();
-
-	double dist = 0.95 * topWid / 2.0 - legThick / 2.0;
-
-	glPushMatrix();
-
-	glTranslated(dist, 0, dist);
-	tableLeg(legThick, legLen);
-
-	glTranslated(0.0, 0.0, -2 * dist);
-	tableLeg(legThick, legLen);
-
-	glTranslated(-2 * dist, 0, 2 * dist);
-	tableLeg(legThick, legLen);
-
-	glTranslated(0, 0, -2 * dist);
-	tableLeg(legThick, legLen);
-
-	glPopMatrix();
-}
-
-void displaySolid()
-{
-	GLfloat mat_ambient[] = { 0.7f, 0.7f, 0.7f, 1.0f };
-	GLfloat mat_diffuse[] = { 0.5f, 0.5f, 0.5f, 1.0f };
-	GLfloat mat_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-	GLfloat mat_shininess[] = { 50.0f };
-	glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
-
-	GLfloat lightIntensity[] = { 0.7f, 0.7f, 0.7f, 1.0f };
-	GLfloat light_Position[] = { 2.0f, 6.0f, 3.0f, 0.0f };
-	glLightfv(GL_LIGHT0, GL_POSITION, light_Position);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightIntensity);
-	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	double winHt = 1.0;
-	glOrtho(-winHt * 64 / 48.0, winHt * 64 / 48.0, -winHt, winHt, 0.1, 100.0);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(2.3, 1.3, 2.0, 0.0, 0.25, 0.0, 0.0, 1.0, 0.0);
+}
 
+void display()
+{
+	glViewport(0, 500, 500, 500);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glPushMatrix();
-	glTranslated(0.6, 0.38, 0.5);
-	glRotated(30, 0, 1, 0);
-	glutSolidTeapot(0.08);
-	glPopMatrix();
-
-	glPushMatrix();
-	glTranslated(0.4, 0, 0.4);
-	table(0.6, 0.02, 0.02, 0.3);
-	glPopMatrix();
-
-	wall(0.02);
-	glPushMatrix();
-	glRotated(90.0, 0.0, 0.0, 1.0);
-	wall(0.02);
-	glRotated(90.0, 1.0, 0.0, 0.0);
-	wall(0.02);
-	glPopMatrix();
+	obj(0, 0, 0.5, 1, 1, 0.04);
+	obj(-0.5, 0, 0, 0.04, 1, 1);
+	obj(0, -3.0, 0, 0.02, 0.2, 0.02);
+	obj(0, -0.3, -0.4, 0.02, 0.2, 0.02);
+	obj(0.4, -0.3, 0, 0.02, 0.2, 0.02);
+	obj(0.4, -0.3, -0.4, 0.02, 0.2, 0.02);
+	obj(0.2, -0.18, -0.2, 0.8, 0.02, 0.8);
+	obj(0, -0.5, 0.02, 1, 0.02, 1);
+	glTranslated(0.03, -0.2, -0.5);
+	glutSolidTeapot(0.1);
+	glLoadIdentity();
 	glFlush();
 }
+
 void main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(640, 480);
-	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Simple shaded scene of a teapot on a table");
-	glutDisplayFunc(displaySolid);
+	GLfloat ambient[] = { 0.3, 0.4, 0.5, 1 };
+	GLfloat Light_pos[] = { 27, 80, 2, 3 };
+	glutInitWindowSize(1000, 1000);
+	glutInitWindowPosition(10, 10);
+	glutCreateWindow("Teapot and Table");
+	glutDisplayFunc(display);
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
-	glShadeModel(GL_SMOOTH);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, ambient);
+	glLightfv(GL_LIGHT0, GL_POSITION, Light_pos);
 	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_NORMALIZE);
-	glClearColor(0.1, 0.1, 0.1, 0.0);
-	glViewport(0, 0, 640, 480);
 	glutMainLoop();
 }
